455299a43d0d5349c8011939d8c3267dcea21b1a
Keaton Robert Gallagher <keaton@keaton.local>
2019-11-11 16:23:27 -0700

    Added a fetch using the package isomorphic-unfetch so that we can make calls, also added getInitial props to my about class component

--- pages/about.js
+++ pages/about.js
@@ -1,15 +1,36 @@
-import Layout from '../components/Layout'
+import React, { Component } from 'react';
+import Layout from '../components/Layout';
+import fetch from 'isomorphic-unfetch';
+export default class About extends Component {
 
-export default () => {
-	return (
-		<Layout title="About Me">
-			<p>
+  static async getInitialProps() {
+     const res = await fetch("https://api.github.com/users/kgallagher52");
+     const data = await res.json();
+      return { user:data }
+  }
+  render() {
+    const { user } = this.props;
+    
+    return (
+      <Layout title="About Me">
+  <p>{user.name}</p>
+    <p>Bio: {user.bio}</p>
+  			<p>
 				I am an extroverted person. I am always up for a new challenge, and it is the best way to grow. I
 				graduated with my bachelors from Dixie State University, in Computer Information Technology, with an
 				emphasis in Web Development. I am eager to learn and improve myself as a human. If you are not
 				struggling, you are not learning.
 			</p>
-			<img src="static/languages.jpeg" alt="different languages" height="200px"/>
+      
+			<img src={user.avatar_url} alt="keaton avatar" height="200px"/>
+      <style jsx>{`
+      p {
+        width:80%;
+      }
+    `}
+    </style>
 		</Layout>
+   
 	);
 };
+}
