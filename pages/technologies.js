1acf76302467d887b61aa22bbd2d67083b5739f2
keaton <keatonrgallagher@gmail.com>
2019-11-11 22:46:31 -0700

    added style

--- pages/technologies.js
+++ pages/technologies.js
@@ -4,41 +4,60 @@ import Link from "next/link";
 import Tech from "./tech";
 import { getTechData } from "../actions";
 
-const Technologies = ({data}) => {
+const Technologies = ({ data }) => {
   return (
     <Layout title="My Technologies">
-      <ul>
+      <div className="outer-container">
         {data.map(t => (
-          <li key={t.id}>
+          <div className="container" key={t.id}>
             <Link as={`/tech${t.id}`} href={`/tech?id=${t.id}`}>
               <a>{t.title}</a>
             </Link>
-          </li>
+          </div>
         ))}
-      </ul>
+      </div>
       <style jsx>
         {`
-          ul {
-            text-decoration: none;
-            color: black;
-            list-style: none;
-            outline: none;
+          .outer-container {
+            display: flex;
+            justify-content: center;
+            width: 100%;
+            flex-flow: row wrap;
           }
-          li,
-          a {
+          .container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            text-align: center;
+            width: 120px;
+            height: 100px;
+            margin: 12px;
+            -webkit-box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+            -moz-box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+            transition:ease-in-out 0.4s;
+            
+          }
+          .container:hover {
+            background: lightgray;
+            -webkit-box-shadow: none;
+            -moz-box-shadow: none;
+            box-shadow: none;
+          }
+          .container a {
+            font-weight:900;
+            font-size:18px;
             text-decoration: none;
             color: black;
-            list-style: none;
-            outline: none;
+            padding:8px;
           }
         `}
       </style>
     </Layout>
-    
   );
 };
 Technologies.getInitialProps = async () => {
-	const data = await getTechData();
-	return {data};
+  const data = await getTechData();
+  return { data };
 };
 export default Technologies;
